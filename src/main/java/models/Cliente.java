3f5b34d080444af575ef91f3f6e8cc407d2447ad
Ester Trevisan <ester.trevisan@academico.ifpb.edu.br>
2024-08-28 15:50:33 -0300

    :sparkles: feat: adicionando classes para usuário e administrador

--- src/main/java/models/Cliente.java
+++ src/main/java/models/Cliente.java
@@ -1,58 +1,9 @@
 package models;
 
-public class Cliente {
-    private String nome;
-    private String email;
-    private String senha;
-    private String telefone;
-    private String CPF;
+public class Cliente extends Usuario{
+    private static final boolean admin = false;
     
-    public String getSenha() {
-        return senha;
+    public boolean isAdmin() {
+        return admin;
     }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getCPF(){
-        return CPF;
-    }
-
-    public void setCPF(String CPF){
-        this.CPF = CPF;
-    }
-    
-   public String toString(){
-        return this.nome + this.CPF + this.email + this.telefone;
-   }
-    
-
-
-
 }
