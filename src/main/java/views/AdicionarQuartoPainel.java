39849e12b3059e16efa6ba7ed998ef649a86eb0d
Otavio <otavioestendio@gmail.com>
2024-08-31 10:20:55 -0300

    adicionando funcionalidades nas telas adicionarQuarto e atualizarCliente

--- src/main/java/views/AdicionarQuartoPainel.java
+++ src/main/java/views/AdicionarQuartoPainel.java
@@ -1,9 +1,45 @@
 package views;
 
+import views.ObjetosTelas.Botao;
+import views.ObjetosTelas.EspacoSenha;
+import views.ObjetosTelas.EspacoTexto;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class AdicionarQuartoPainel extends PainelPadrao{
-    public AdicionarQuartoPainel(){
+    private EspacoTexto numero = new EspacoTexto();
+    private EspacoTexto categoria = new EspacoTexto();
+    private EspacoTexto andar = new EspacoTexto();
+    private EspacoTexto precoDiaria = new EspacoTexto();
+    private Botao adicionar = new Botao(false);
 
+    public AdicionarQuartoPainel(){
+        objetos();
+        ouvinte();
+        fundo("Adicionar quarto");
+    }
+
+    private void ouvinte() {
+        adicionar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    private void objetos() {
+        int a = 363, c = 345,d = 57;
+        numero.setBounds(a,196,c,d);
+        categoria.setBounds(a,290,c,d);
+        andar.setBounds(a,384,c,d);
+        precoDiaria.setBounds(a,479,c,d);
+        adicionar.setBounds(409,565,252,72);
+        add(numero);
+        add(categoria);
+        add(andar);
+        add(precoDiaria);
+        add(adicionar);
     }
 }
--- src/main/java/views/AtualizarCliente.java
+++ src/main/java/views/AtualizarCliente.java
@@ -1,9 +1,74 @@
 package views;
 
+import views.ObjetosTelas.Botao;
+import views.ObjetosTelas.EspacoTexto;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class AtualizarCliente extends PainelPadrao{
+    private EspacoTexto nome = new EspacoTexto();
+    private EspacoTexto email = new EspacoTexto();
+    private EspacoTexto senha = new EspacoTexto();
+    private EspacoTexto telefone = new EspacoTexto();
+    private EspacoTexto cpf = new EspacoTexto();
+    private EspacoTexto pesquisar = new EspacoTexto();
+    private Botao atualizar = new Botao(false);
+    private Botao limpar = new Botao(false);
+    private Botao lupa = new Botao(false);
+
+
+
     public AtualizarCliente(){
+        objetos();
+        ouvintes();
         fundo("atualizarCliente");
+    }
+
+    private void ouvintes() {
+        atualizar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        limpar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                nome.setText("");
+                email.setText("");
+                senha.setText("");
+                telefone.setText("");
+                cpf.setText("");
+
+            }
+        });
+        lupa.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+
+    private void objetos() {
+        int a = 193,c = 299,d = 50;
+        nome.setBounds(a,173,c,d);
+        email.setBounds(a,253,c,d);
+        senha.setBounds(a,333,c,d);
+        telefone.setBounds(635,173,c,d);
+        cpf.setBounds(635,253,c,d);
+        pesquisar.setBounds(158,80,812,63);
+        atualizar.setBounds(192,510,296,85);
+        limpar.setBounds(584,510,296,85);
+        lupa.setBounds(101,83,56,56);
+        add(nome);
+        add(email);
+        add(senha);
+        add(telefone);
+        add(cpf);
+        add(pesquisar);
+        add(atualizar);
+        add(limpar);
+        add(lupa);
     }
 }
