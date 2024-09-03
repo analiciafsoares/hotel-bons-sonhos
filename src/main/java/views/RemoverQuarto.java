d82908feb9c20c4c436a099d0f92d75ec232bf82
Otavio <otavioestendio@gmail.com>
2024-09-03 19:22:49 -0300

    feita conexão entre telas

--- src/main/java/views/RemoverQuarto.java
+++ src/main/java/views/RemoverQuarto.java
@@ -1,9 +1,41 @@
 package views;
 
+import views.ObjetosTelas.Botao;
+import views.ObjetosTelas.EspacoTexto;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class RemoverQuarto extends PainelPadrao{
-    public RemoverQuarto(){
+    private EspacoTexto numero = new EspacoTexto();
+    private EspacoTexto categoria = new EspacoTexto();
+    private EspacoTexto andar = new EspacoTexto();
+    private Botao remover = new Botao(false);
 
+    public RemoverQuarto(){
+        objetos();
+        ouvintes();
+        fundo("remover Quarto");
+    }
+
+    private void ouvintes() {
+        remover.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    private void objetos() {
+        int a = 373, c = 324, d = 50;
+        numero.setBounds(a,238,c,d);
+        categoria.setBounds(a,332,c,d);
+        andar.setBounds(a,426,c,d);
+        remover.setBounds(409,547,252,71);
+        add(numero);
+        add(categoria);
+        add(andar);
+        add(remover);
     }
 }
