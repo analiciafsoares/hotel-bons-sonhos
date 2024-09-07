63831dcc480f5beffbec9b3099b4acd047019c3b
Otavio <otavioestendio@gmail.com>
2024-09-07 11:08:40 -0300

    Começando a tela menu de cliente

---
+++ src/main/java/views/telasCliente/TelaMenuCliente.java
@@ -0,0 +1,14 @@
+package views.telasCliente;
+
+import views.JanelaPadrao;
+import views.ObjetosTelas.Botao;
+
+public class TelaMenuCliente extends JanelaPadrao {
+    public TelaMenuCliente(){
+        fundo("menuCliente");
+    }
+
+    public static void main(String[] args) {
+        new TelaMenuCliente();
+    }
+}
