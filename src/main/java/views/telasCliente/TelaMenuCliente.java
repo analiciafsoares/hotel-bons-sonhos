0dd9ae3221ee40a8bf1d66ae111673b4aba8ff2a
Otavio <otavioestendio@gmail.com>
2024-09-09 23:13:06 -0300

    tela Menu Cliente

--- src/main/java/views/telasCliente/TelaMenuCliente.java
+++ src/main/java/views/telasCliente/TelaMenuCliente.java
@@ -3,9 +3,41 @@ package views.telasCliente;
 import views.JanelaPadrao;
 import views.ObjetosTelas.Botao;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class TelaMenuCliente extends JanelaPadrao {
+    private Botao buscar = new Botao(false);
+    private Botao visualizar = new Botao(false);
+
+
+
     public TelaMenuCliente(){
+        objetos();
+        ouvintes();
         fundo("menuCliente");
+    }
+
+    private void ouvintes() {
+        buscar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        visualizar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+    }
+
+    private void objetos() {
+        buscar.setBounds(174,260,382,238);
+        visualizar.setBounds(731,260,382,238);
+        add(buscar);
+        add(visualizar);
     }
 
     public static void main(String[] args) {
